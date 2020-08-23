0bab75ac222a65e495bdc921093a8d34d8b7db27
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-08-23 13:12:57 +0900

    関数追加

--- getById.js
+++ getById.js
@@ -4,3 +4,14 @@ function onHlButtonClick(){
     let headTitle = document.getElementById("headTitle");
     console.log(headTitle);
 }
+
+function onHlTextButtonClick(){
+    let headTitle =document.getElementById("headTitle");
+    let headTItleText = headTitle.textContent;
+    console.log(headTItleText);
+}
+
+function onHlTextAltButtonClick(){
+    let headTitle =document.getElementById("headTitle");
+    headTitle.textContent ="idによる取得sample";
+}
