6c002b67a5ee650ecebb95a2375ea3d7c1571b60
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-08-23 15:12:57 +0900

    tagbutton

---
+++ getElement.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function onTagButtonClick(){
+    let buttonTags = document.getElementsByTagName("button")
+
+    let result="";
+
+    for(let i =0;i < buttonTags.length; i++) {
+        result += buttonTags[i].textContent + ":";
+    }
+
+    let resultP =document.getElementById("result");
+
+    resultP.textContent =result;
+}
+function onClassButtonClick(){
+    
+}
+//onNameButtonClick()
+
+
+
